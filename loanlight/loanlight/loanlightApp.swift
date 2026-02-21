9ae37aab98c67f65de146124f03dc0a00e622e57
shreeya kantamsetty <saishreeya@gmail.com>
2026-02-21 15:07:17 -0500

    starting

---
+++ loanlight/loanlight/loanlightApp.swift
@@ -0,0 +1,17 @@
+//
+//  loanlightApp.swift
+//  loanlight
+//
+//  Created by Shreeya Kantamsetty on 2/21/26.
+//
+
+import SwiftUI
+
+@main
+struct loanlightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
